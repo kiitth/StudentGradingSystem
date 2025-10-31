57d7901b81d1758c5896cd736fe26fae9d934e4a
andrewassoh <andrew716a@gmail.com>
2025-10-30 22:31:46 -0400

    added foreign key constraints

--- Student_Grading_Sytem/student_grading_system.sql
+++ Student_Grading_Sytem/student_grading_system.sql
@@ -17,7 +17,8 @@ CREATE TABLE students (
 CREATE TABLE courses (
   course_id INT AUTO_INCREMENT PRIMARY KEY,
   course_name VARCHAR(100),
-  teacher_id INT
+  teacher_id INT,
+  FOREIGN KEY (teacher_id) REFERENCES users(id)
 );
 
 CREATE TABLE grades (
@@ -25,13 +26,18 @@ CREATE TABLE grades (
   student_id INT,
   course_id INT,
   grade VARCHAR(5),
-  date_submitted DATE
+  date_submitted DATE,
+  FOREIGN KEY (student_id) REFERENCES students(student_id),
+  FOREIGN KEY (course_id) REFERENCES courses(course_id)
 );
 
 CREATE TABLE attendance (
   id INT AUTO_INCREMENT PRIMARY KEY,
   student_id INT,
-  course_id VARCHAR(20),
+  course_id INT,
   status ENUM('Present','Absent','Late'),
-  date DATE
+  date DATE,
+  FOREIGN KEY (student_id) REFERENCES students(student_id),
+  FOREIGN KEY (course_id) REFERENCES courses(course_id)
 );
+
